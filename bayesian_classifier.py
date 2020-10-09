3fd5a1558419125bb3bc711c1c3eb32105e683fa
maksprotsyk <maksprotsyk@ucu.edu.ua>
2020-10-09 17:53:23 +0300

    Fixed everything

--- bayesian_classifier.py
+++ bayesian_classifier.py
@@ -8,106 +8,105 @@ class BayesianClassifier:
     Implementation of Naive Bayes classification algorithm.
     """
 
-    def __init__(self):
-        self.features = {}
-        self.label_probs = {}  # P(class)
-        self.feature_probs = []  # P(feature)
-        self.conditional_probs = {}  # P(feature | class)
-        self.correct_tests = 0
+    def __init__(self) -> None:
+        """
+        Initializes BayesianClassifier with label, features set to None
+        and total score set to 0
+        """
+        self.labels = None
+        self.features = None
+        self.total_score = 0
 
-    def fit(self, X, y):
+    def fit(self, X: pd.DataFrame, y: pd.DataFrame) -> None:
         """
         Fit Naive Bayes parameters according to train data X and y.
-        :param X: pd.DataFrame|list - train input/messages
-        :param y: pd.DataFrame|list - train output/labels
-        :return: None
         """
-        self.features = {feature: ind for ind, feature
-                         in enumerate(np.unique(np.hstack(X['Processed Tweet'])))}
-        labels = y['label'].unique()
-        self.label_probs = {label: 0 for label in labels}
-        self.conditional_probs = {label: [0] * len(self.features) for label in labels}
+        self.total_score = 0
+        words = np.unique(np.hstack(X['Processed Tweet']))
+        df_data = {'Feature': words, 'Index': range(len(words))}
+        self.labels = {label: len(y[y['label'] == label]) / len(y)
+                       for label in y['label'].unique()}
 
-        # print(f"self.features - {self.features}")
-        # print(len(self.features))
+        df_data.update({label: [0] * len(words) for label in self.labels})
+        self.features = pd.DataFrame(data=df_data).set_index("Feature")
+        self.data_size = len(X)
 
         def check_features(row):
+            """
+            Calculates occurences of all feature words
+            in the column of tweets
+            """
             processed_tweet = row["Processed Tweet"]
-            for word in set(processed_tweet):
-                self.conditional_probs[row['label']][self.features[word]] += 1
-            self.label_probs[row['label']] += 1
-            # print("_____________________________________________________________________________________\n\n")
-            # print(row)
+            for word in processed_tweet:
+                self.features.loc[word][row['label']] += 1
             return row
 
-        df = X.merge(y, left_index=True, right_index=True).apply(check_features, axis=1)
+        df = X\
+             .merge(y, left_index=True, right_index=True)\
+             .apply(check_features, axis=1)
+        for label in self.labels:
+            self.features[label] = self.features[label].astype(float)
 
-        for label in self.label_probs:
-            self.conditional_probs[label] = [prob / self.label_probs[label]
-                                             for prob in self.conditional_probs[label]]
-            self.label_probs[label] /= len(df)
-        self.feature_probs = [0] * len(self.features)
-        for count in range(len(self.features)):
-            self.feature_probs[count] = sum([self.conditional_probs[label][count] * self.label_probs[label]
-                                             for label in self.label_probs])
+        def apply_log(row, big_num=-100000000):
+            """
+            Converts all numbers of occurrences
+            to their log2
+            (if number is 0 converts it to a
+            big negative number)
+            """
+            for label in self.labels:
+                if row[label] == 0.0:
+                    row[label] = big_num
+                else:
+                    row[label] = np.log2(row[label])
+            return row
 
-        # print(self.features)
-        # print(self.feature_probs)
-        # print(self.label_probs)
-        # print(self.conditional_probs)
+        self.features = self.features.apply(apply_log, axis=1)
 
-    def predict_prob(self, message, label):
+        for label in self.labels:
+            self.features[label] -= np.log2(len(y[y['label'] == label]))
+
+    def predict_prob(self, message: str, label: str) -> float:
         """
-        Calculate the probability that a given label can be assigned to a given message.
-        :param message: str - input message
-        :param label: str - label
-        :return: float - probability P(label|message)
+        Calculate the log2 of probability
+        that a given label can be assigned to a given message.
         """
-        tweet_words = {word for word in re.sub(r"[\W\d]", r" ", message).split()
-                       if word in self.features}
-        features = [0] * len(self.features)
+        message = re.sub(r"[^a-zA-Z\']", r" ", message)
+        message = re.sub(r"(.)\1(\1+)", r"\1", message)
+        tweet_words = {word for word in message.split()
+                       if word in self.features.index.values.tolist()}
+        conditional_prob = 0
         for word in tweet_words:
-            features[self.features[word]] = 1
-        condititional_prob = 1
-        total_prob = 1
-        for count in range(len(features)):
-            if features[count] == 1:
-                condititional_prob *= self.conditional_probs[label][count]
-                total_prob *= self.feature_probs[count]
-            else:
-                condititional_prob *= 1 - self.conditional_probs[label][count]
-                total_prob *= 1 - self.feature_probs[count]
-        # print(condititional_prob * self.label_probs[label])
-        # print(condititional_prob * self.label_probs[label] / total_prob)
-        return condititional_prob * self.label_probs[label] / total_prob
+            conditional_prob += (self.features.loc[word][label]) + np.log2(self.labels[label])
+        return conditional_prob - np.log2(self.data_size)
 
-    def predict(self, message):
+
+    def predict(self, message: str) -> str:
         """
         Predict label for a given message.
-        :param message: str - message
-        :return: str - label that is most likely to be truly assigned to a given message
         """
-        answer = ('0', 0)
-        for lable in self.conditional_probs.keys():
-            lable_prob = self.predict_prob(message, lable)
-            if lable_prob > answer[1]:
-                answer = (lable, lable_prob)
-        return answer[0]
+        print(message)
+        label_probs = [(label, self.predict_prob(message, label)) for label in self.labels]
+        print(label_probs)
+        max_value = max(label_probs, key=lambda x: x[1])[1]
+        max_labels = [item[0] for item in label_probs if item[1] == max_value]
 
-    def test_prediction(self, row):
-        if self.predict(' '.join(row['Processed Tweet'])) == row['label']:
-            self.correct_tests += 1
-        return row
+        return max(max_labels, key=lambda x: self.labels[x])
 
-    def score(self, X, y):
+
+    def score(self, X: pd.DataFrame, y: pd. DataFrame) -> float:
         """
-        Return the mean accuracy on the given test data and labels - the efficiency of a trained model.
-        :param X: pd.DataFrame|list - test data - messages
-        :param y: pd.DataFrame|list - test labels
-        :return:
+        Return the mean accuracy on the given test data
+        and labels - the efficiency of a trained model.
         """
-        self.correct_tests = 0
-        df = X.merge(y, left_index=True, right_index=True).apply(self.test_prediction, axis=1)
-        score = self.correct_tests/len(df)
-        return score
+        self.total_score = 0
+        def test_prediction(row):
+            """
+            Tests prediction for the given row
+            """
+            if self.predict(row['tweet']) == row['label']:
+                self.total_score += 1
+
+        df = X.merge(y, left_index=True, right_index=True).apply(test_prediction, axis=1)
+        return self.total_score/len(df)
 
--- main.py
+++ main.py
@@ -1,50 +1,70 @@
 from bayesian_classifier import BayesianClassifier
-import pandas as pd
 import re
+import pandas as pd
+
+with open('stop_words.txt') as in_file:
+    STOP_WORDS = tuple(in_file.read().split('\n'))
 
 
-def process_data(data_file, stop_words_file="stop_words.txt"):
+def process_train_data(data_file, stop_words=STOP_WORDS):
     """
-    Function for data processing and split it into X and y sets.
+    Function for training data processing and splitting it into X and y sets.
     :param stop_words_file:
     :param data_file: str - train data
     :return: pd.DataFrame|list, pd.DataFrame|list - X and y data frames or lists
     """
-    with open(stop_words_file) as in_file:
-        stop_words = in_file.read().split('\n')
-    word_frequencies = {}
 
+    word_discrim = set()
     def clear_text(row, min_len=4):
-        text = re.sub(r"[\W\d]", r" ", row["tweet"])
-        row["Processed Tweet"] = [word.lower() for word in text.split()
-                                  if word.lower() not in stop_words and len(word) >= min_len]
-        for word in row["Processed Tweet"]:
-            word_frequencies[word] = word_frequencies.get(word, 0) + 1
+        """
+        Clears text from unnecessary
+        information and words of length 3 or less
+        """
+        text = re.sub(r"[^a-zA-Z]", r" ", row["tweet"])
+        text = re.sub(r"(.)\1(\1+)", r"\1", text)
+        row["Processed Tweet"] = [word for word in text.lower().split()
+                                  if word not in stop_words and word != 'user' and
+                                  len(word) >= min_len]
+        if row['label'] == 'discrim':
+            for word in row["Processed Tweet"]:
+                word_discrim.add(word)
         return row
 
-    def remove_infrequent(row, min_freq=10):
+    def remove_neutral_words(row):
+        """
+        Removes words that don't appear in
+        discriminating tweets (for better accuracy)
+        """
         row["Processed Tweet"] = [word for word in row["Processed Tweet"]
-                                  if word_frequencies[word] >= min_freq]
+                                  if word in word_discrim]
         return row
 
     df = pd \
         .read_csv(data_file, encoding="utf8") \
         .apply(clear_text, axis=1) \
         .drop(labels=["tweet", "id", "Unnamed: 0"], axis=1) \
-        .apply(remove_infrequent, axis=1)
+        .apply(remove_neutral_words, axis=1)
 
     return df.drop("label", axis=1), df.drop("Processed Tweet", axis=1)
 
 
+
 def test_BayesianClassifier():
-    train_X, train_y = process_data("train.csv")
+    """
+    Trains Bayesian Classifier on test data and then tests it
+    """
+    train_X, train_y = process_train_data("train.csv")
     classifier = BayesianClassifier()
     classifier.fit(train_X, train_y)
-    test_X, test_y = process_data("test.csv")
-    print("model score: ", classifier.score(test_X, test_y)*100, "%")
+    test_data = pd \
+                .read_csv("test.csv", encoding="utf8") \
+                .drop(labels=["id", "Unnamed: 0"], axis=1)
+    test_X = test_data.drop("label", axis=1)
+    test_y = test_data.drop("tweet", axis=1)
+
+    print("model score: ", classifier.score(test_X, test_y) * 100, "%")
+
 
 
 if __name__ == "__main__":
     test_BayesianClassifier()
-    # classifier.predict_prob("classiest show in the wilderness #magickingdom #countrybearjamboree   @ country bearâ¦ ", "neutral")
-    # print(process_data("train.csv"))
